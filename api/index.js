461a0e49aa6f17d44283328dec286e74e5b03ab3
canan can <ciuniliusmasih@gmail.com>
2026-03-10 18:49:48 +0500

    .env

--- api/index.js
+++ api/index.js
@@ -1,20 +1,28 @@
 const express = require("express")
 const app = express();
-let PORT =  process.env.PORT || 2000
+let PORT = process.env.PORT || 2000
 let path = require("path");
-const bodyParser = require("body-parser")
-const hbs = require("handlebars");
+require("dotenv").config({path:".env"});
+// # DATABASE_URI=mongodb+srv://cluster0.ymtzfu0.mongodb.net/" --apiVersion 1 --username canan"
+
+const dbFunc = require("../db/conn");
+
+// const bodyParser = require("body-parser")
+// const hbs = require("handlebars");
 // let PORT =   2000
 // bodyParser
 // app.set()
 // console.log(path.join(__dirname,  "../template"))
-let staticPath = path.join(__dirname,  "../static")
+let staticPath = path.join(__dirname, "../static")
 // let handbarPath = path.join(__dirname,  "../template/partials")
 // console.log(handbarPath)
-let tempPath = path.join(__dirname,  "../template/views");
-console.log(tempPath)
+let tempPath = path.join(__dirname, "../template/views");
+// console.log(tempPath)
 express.static(staticPath)
 const fs = require("fs");
+const contactModel = require("../models/Contact");
+
+
 // hbs.registerPartial(`${__dirname}../template/partials`)
 // app.use(express.static(statPath));
 // app.use(express.static(tempPath));
@@ -23,6 +31,9 @@ const fs = require("fs");
 app.use("/static", express.static(staticPath));
 app.set("view engine", "html");
 app.set("views", tempPath);
+// app.use(express.json())
+// app.use(express.urlencoded())
+app.use(express.urlencoded({}))
 
 // app.use(hbs.registerPartial(handbarPath))
 // app.use(handbarPath)
@@ -36,9 +47,9 @@ app.set("views", tempPath);
 
 // app.use("views", tempPath);
 // app.set("views", "html");
-app.engine("html", function(path, option, cb){
-    fs.readFile(path,(err,data)=>{
-        if(err){
+app.engine("html", function (path, option, cb) {
+    fs.readFile(path, (err, data) => {
+        if (err) {
             // cb(err);
             return err
         }
@@ -55,47 +66,52 @@ app.engine("html", function(path, option, cb){
 // app.use()
 // console.log(staticPath)
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.status(200).render("index");
 });
-app.get("/about", (req,res)=>{
+app.get("/about", (req, res) => {
     res.status(200).render('about')
 });
-app.get("/services", (req,res)=>{
-    // res.send("Hello World")
+app.get("/services", (req, res) => {
     res.status(200).render("services");
 
 });
-app.get("/contacts", (req,res)=>{
-    // res.send("Hello World")
+app.get("/contacts", (req, res) => {
     res.status(200).render("contact");
 
 });
-app.post("/contact", (req,res)=>{
-    res.send("Post Contact")
+app.post("/contact", async (req, res) => {
+    try {
+        await contactModel.create(req.body)
+        res.status(201).json({ msg: "Thank You for Contact form Us" })
+
+    } catch (err) {
+        console.log('ThE Err',err)
+
+    }
 });
-app.get("/project", (req,res)=>{
-    // res.send("Hello World")
+app.get("/project", (req, res) => {
     res.status(200).render("project");
 
 });
-app.get("/reviews", (req,res)=>{
-    // res.send("Hello World")
-
+app.get("/reviews", (req, res) => {
     res.status(200).render("review");
 });
 
-app.get("/404", (req,res)=>{
+// app.get("/*", (req, res) => {
+// app.get("*", (req, res) => {
+app.get("/error", (req, res) => {
     try {
         // res.status(404).send("Error")
         res.status(404).redirect('/')
         // res.status(404).render('404')
-        
+
     } catch (error) {
         console.log('Th Err', error)
     }
 })
-
-app.listen(PORT, ()=>{
-    console.log('I am live to the port '+PORT)
+dbFunc().then(() => {
+    app.listen(PORT, () => {
+        console.log('I am live to the port ' + PORT)
+    });
 });
